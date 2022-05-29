ffe73cc7f2655198702c9781c413687aee8b514f
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2022-05-29 22:58:16 +1200

    Refactor calculation of running total
    Also, introduce some comments and clean up formatting

--- Checkout.py
+++ Checkout.py
@@ -7,25 +7,26 @@ class Checkout:
 
   def scan(self, item):
     """
-        Scan an item into the checkout.
-        """
-    if item in self.scannedItems:
-      self.scannedItems[item] += 1
+    Scan an item into the checkout.
+    """
+    name = item.name
+
+    if name in self.scannedItems:
+      self.scannedItems[name]['amount'] += 1
     else:
-      self.scannedItems[item] = 1
+      self.scannedItems[name] = {'amount': 1, 'price': 0}
+
+    amount = self.scannedItems[name]['amount']
+
+    self.scannedItems[item.name]['price'] = self.rules.getPrice(name, amount)
+
     self._updateTotal()
 
   def _updateTotal(self):
     """
-        Based on the scanned items, update the running total
-        using the information from the pricing rules.
-
-        Refactor note: Why calculate total from scratch
-        each time? If there's a way to only update the total
-        based on the most recently scanned item then it
-        makes an O(n) algorithm into an O(1) algorithm
-        """
+    Construct an updated total from the currently scanned items.
+    """
     self.total = 0
-    for item, amount in self.scannedItems.items():
-      price = self.rules.getPrice(item, amount)
-      self.total += price
+
+    for item in self.scannedItems.values():
+      self.total += item['price']
--- Rules.py
+++ Rules.py
@@ -8,13 +8,28 @@ class Rules():
       self.pricing = json.load(f)
 
   def getPrice(self, item, amount):
+    """
+    Get the price of an item, given the
+    name of the item and the amount of the
+    item.
+
+    This references the pricing policy to
+    apply deals based on the amount of an
+    item present.
+    """
     price = 0
-    if item.name in self.pricing:
+
+    if item in self.pricing:
+      # Transform the pricing data so that
+      # prices for amounts of items are in
+      # descending order
       sorted_prices = dict(
-          sorted(self.pricing[item.name].items(),
-                 key=lambda item: item[1],
-                 reverse=True))
+        sorted(self.pricing[item].items(),
+               key=lambda item: item[1],
+               reverse=True))
+
       for n in sorted_prices:
         price += (amount // int(n)) * sorted_prices[n]
         amount = amount % int(n)
+
     return price
