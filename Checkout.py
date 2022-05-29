5b27c9258d9addb16b0b3a072876f22fa5d0c2d1
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2022-05-29 22:07:48 +1200

    Format with yapf

--- Checkout.py
+++ Checkout.py
@@ -1,21 +1,22 @@
 class Checkout:
-    def __init__(self, rules):
-        self.rules = rules
-        self.total = 0
-        self.scannedItems = dict()
 
-    def scan(self, item):
-        """
+  def __init__(self, rules):
+    self.rules = rules
+    self.total = 0
+    self.scannedItems = dict()
+
+  def scan(self, item):
+    """
         Scan an item into the checkout.
         """
-        if item in self.scannedItems:
-            self.scannedItems[item] += 1
-        else:
-            self.scannedItems[item] = 1
-        self._updateTotal()
+    if item in self.scannedItems:
+      self.scannedItems[item] += 1
+    else:
+      self.scannedItems[item] = 1
+    self._updateTotal()
 
-    def _updateTotal(self):
-        """
+  def _updateTotal(self):
+    """
         Based on the scanned items, update the running total
         using the information from the pricing rules.
 
@@ -24,7 +25,7 @@ class Checkout:
         based on the most recently scanned item then it
         makes an O(n) algorithm into an O(1) algorithm
         """
-        self.total = 0
-        for item, amount in self.scannedItems.items():
-            price = self.rules.getPrice(item, amount)
-            self.total += price
+    self.total = 0
+    for item, amount in self.scannedItems.items():
+      price = self.rules.getPrice(item, amount)
+      self.total += price
